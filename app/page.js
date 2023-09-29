3a4b267dba91a1c40b3f0f1cbe6017e73f2563fe
aceytok <ace.manuyag@edu.sait.ca>
2023-09-28 21:42:18 -0600

    updated css

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import StudentInfo from "./StudentInfo.js";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-t from-blue-100 via-blue-300 to-blue-500">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
         <StudentInfo />
--- app/week2/page.js
+++ app/week2/page.js
@@ -3,7 +3,7 @@ import StudentInfo from "../StudentInfo.js";
 
 export default function HomeWeek2() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-t from-blue-100 via-blue-300 to-blue-500">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <h1 className="text-4xl">My Shopping List</h1>
                 <StudentInfo />
