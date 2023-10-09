7bc8271d4124e5d5f00b4923a6789f5254310b4a
aceytok <ace.manuyag@edu.sait.ca>
2023-10-08 22:51:05 -0600

    changed css on week3

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,10 +1,9 @@
 export default function Item({name, quantity, category}) {
     return (
       <ul>
-        <li class="p-2 m-4 bg-slate-900 max-w-sm">
+        <li class="p-2 m-4 bg-gradient-to-l from-gray-700 via-gray-900 to-black max-w-sm">
           <h3 class="text-xl font-bold">{name}</h3> 
-          <p>Quantity: {quantity}</p> 
-          <p>In {category} aisle</p> 
+          <p class="text-sm">Buy {quantity} in {category}</p> 
         </li>
       </ul>
   
